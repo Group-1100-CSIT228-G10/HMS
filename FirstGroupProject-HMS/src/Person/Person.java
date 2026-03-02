2b54d43aa7c312e68b98a68d4dc74cb6fc67612f
Jess <jessvenzel.casinto@cit.edu>
2026-03-03 00:30:58 +0800

    Finished na po ang Guest & Staff System... paki check if nay kuwang

--- FirstGroupProject-HMS/src/Person/Person.java
+++ FirstGroupProject-HMS/src/Person/Person.java
@@ -1,4 +1,55 @@
 package Person;
 
 public abstract class Person {
+    private String id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String contactNumber;
+    private String email;
+
+    public Person(String id, String firstName, String middleName, String lastName,
+                  String contactNumber, String email) {
+
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.contactNumber = contactNumber;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        if (middleName == null || middleName.isEmpty()) {
+            return firstName + " " + lastName;
+        }
+        return firstName + " " + middleName + " " + lastName;
+    }
+
+    
+    public abstract void displayInfo();
 }
